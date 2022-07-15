2142b373aceb36a2f80223aedcb2ba54353152e1
Yurii Chudinovych <yuriichudinovych93@gmail.com>
2022-07-15 21:06:25 +0300

    add div containers for tag "a"

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,14 +3,17 @@ import { galleryItems } from "./gallery-items.js";
 const galleryRef = document.querySelector(".gallery");
 
 const createMarkupElement = ({ preview, original, description }) => {
-  return `<a class="gallery__link" href="${original}">
+  return `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
       alt="${description}"
     />
-  </a>`;
+  </a>
+  </div>`;
 };
 
 const createGallary = (galleryItems, callback) => {
